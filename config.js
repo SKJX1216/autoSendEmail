cc35451b54fa6252b9cdc938aeec536ec8cf3d86
SKJX1216 <skjx1216@gmail.com>
2022-01-06 20:00:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ module.exports = {
   fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
   user: 'pxs_jpp@163.com', // 发送者邮箱
   pass: 'VUSMXUAQTHMLLEKW', // 发送者邮箱MTP协议密码
-  to: 'skjx1216@163.com', // 发送到谁，填邮箱
+  to: '3264685741@qq.com', // 发送到谁，填邮箱
   weatherKey: '3a78fad60f254eee9e802093cd23d746', // 和风天气key
   location: '101190113',  // 和风天气-成都武侯区的id
-  type: '1,3,9', // 和风天气-生活指数type
+  type: '3,5,10', // 和风天气-生活指数type
   tianXingKey: '80691be4abc7d763bb992683fe0cec16', // 天行数据的key
   startDay: '2019-04-05', // 在一起的日期
 }
