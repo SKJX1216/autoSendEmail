cc32bc631865ad76debf012e6e181cc02c2b363c
SKJX1216 <skjx1216@gmail.com>
2022-01-06 18:18:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 // 配置信息
 module.exports = {
-  fromDisplayText: '阳哥', // 收件箱展示的来件人名字
+  fromDisplayText: '平小硕', // 收件箱展示的来件人名字
   fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
-  user: 'xxx', // 发送者邮箱
-  pass: 'xxx', // 发送者邮箱MTP协议密码
-  to: 'xxx', // 发送到谁，填邮箱
-  weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
-  location: 'xxx',  // 和风天气-成都武侯区的id
+  user: 'pxs_jpp@163.com', // 发送者邮箱
+  pass: 'VUSMXUAQTHMLLEKW', // 发送者邮箱MTP协议密码
+  to: 'skjx1216@163.com', // 发送到谁，填邮箱
+  weatherKey: '3a78fad60f254eee9e802093cd23d746', // 和风天气key
+  location: '101190113',  // 和风天气-成都武侯区的id
   type: '1,3,9', // 和风天气-生活指数type
-  tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
-  startDay: '2015-09-29', // 在一起的日期
+  tianXingKey: '80691be4abc7d763bb992683fe0cec16', // 天行数据的key
+  startDay: '2019-04-25', // 在一起的日期
 }
